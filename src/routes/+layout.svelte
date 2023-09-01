d761cc20657762eb6c395c5d8c57065f3bf35613
wlsfmadl3 <136785976+wlsfmadl3@users.noreply.github.com>
2023-09-01 14:49:59 +0900

    ?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,37 @@
+<script>
+
+	import { buildErrorMessage } from "vite";
+
+</script>
 <div id='container'>
 <link rel="stylesheet" href="src/assets/main.css" />	
+<link rel="stylesheet" href="src/assets/line.css" />	
+<link rel="stylesheet" href="src/assets/tables.css" />	
+
     <header class="header">
-     <h1><a href="/">태성고등학교 입시포털</a></h1>
+       <h1 class="round shadow1"style="color: white; background-color: #6666FF; width: 470px;"><a href="/">태성고등학교 입시포털</a></h1>
+       <style>
+        .shadow1{
+            color: white;
+            text-shadow: 7px -7px 20px #000; 
+
+        }
+        .round{
+            border-bottom-right-radius: 20px;
+            border-bottom-right-radius: 20px;
+            border-top-left-radius: 20px;
+            border-top-left-radius: 20px;
+        }
+       </style>
      <div id="nav">
             <ul>
-              <li><a href="/line">생기부 작성 요령</a></li>
-              <li><a href="/gradecut">등급컷 대학 라인업</a></li>
-              <li><a href="/subject">선택과목 가이드</a></li>
-              <li><a href="/test">모의고사 기출 자료실</a></li>
-             <li><a href="/graduate">모교 진학 통계</a></li>
-          </ul>
-      </div>
+                <li><a href="/line">생기부 작성 요령</a></li>
+                <li><a href="/gradecut">등급컷 대학 라인업</a></li>
+                <li><a href="/subject">선택과목 가이드</a></li>
+                <li><a href="/test">모의고사 기출 자료실</a></li>
+                <li><a href="/graduate">모교 진학 통계</a></li>
+            </ul>
+        </div>
     </header>
     <main>
         <div class='contents'>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,2 +1,29 @@
-<h1>test</h1>
-<p>test</p>
+<body>
+<h1>모의고사 기출 자료실</h1>
+<table id="test-table">
+    <tr>
+        <th>모의고사</th>
+        <th>설명</th>
+        <th>다운로드</th>
+    </tr>
+    <tr>
+        <td><a href="모의고사1.pdf" id="hyperlink">모의고사 1</a></td>
+        <td>모의고사 1에 대한 설명</td>
+        <td><a href="모의고사1.pdf" id="hyperlink">다운로드</a></td>
+    </tr>
+    <tr>
+        <td><a href="모의고사2.pdf" id="hyperlink">모의고사 2</a></td>
+        <td>모의고사 2에 대한 설명</td>
+        <td><a href="모의고사2.pdf" id="hyperlink">다운로드</a></td>
+    </tr>
+    <tr>
+        <td><a href="모의고사3.pdf" id="hyperlink">모의고사 3</a></td>
+        <td>모의고사 3에 대한 설명</td>
+        <td><a href="모의고사3.pdf" id="hyperlink">다운로드</a></td>
+    </tr>
+    
+    <!-- 모의고사 더 추가 가능!! -->
+    
+</table>
+
+</body>
